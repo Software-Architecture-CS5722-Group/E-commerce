30f805dad372641ec1f92f21e671f22fa29d4ad4
Aghoghomena <aghoghojao@gmail.com>
2021-05-02 20:05:03 +0100

    Created the Service registry

---
+++ service-registry/src/test/java/com/groupjn/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupjn.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
